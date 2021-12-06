91391db030c6d8f69dccca249816093b767e5d36
John Chong <johnichong@gmail.com>
2021-12-06 13:53:05 -0500

    Fixed alignment and added comments

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,68 +1,65 @@
 import React, { Component } from 'react';
 import {
-  Col, Row,
   Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    renderComments(comments) {
-      if (comments != null) {
-        const commentsList = comments.map((comment) => {
-          const date = comment.date.split('T')[0].split('-');
-          let formattedDate = `${months[date[1]-1]} ${date[2]}, ${date[0]}`;
+  // render the selected dish with image, name, and description
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <div className='col-12 col-md-5 m-1'>
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      )
+    else
+      return (<div></div>)
+  }
 
-          return (
-            <ul className = "list-unstyled">
-              <li>{comment.comment} </li>
-              <li>-- {comment.author}, {formattedDate}</li>
-            </ul>
-          );
-        });
-
-        return (
-            <CardText>
-                {commentsList}
-            </CardText>
-          );
-      }
-      else
-        return(
-            <div></div>
-        );
-    }
-
-    render() {
-        const dish = this.props.dish;
+  // render the comments of selected dish
+  renderComments(comments) {
+    if (comments != null) {
+      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      const commentsList = comments.map((comment) => {
+        const date = comment.date.split('T')[0].split('-');
+        let formattedDate = `${months[date[1]-1]} ${date[2]}, ${date[0]}`;
 
         return (
-          <div className="mt-2">
-            <div className="row">
-              <div className="col">
-                <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-                </Card>
-              </div>
-              <div className="col">
-                <Card>
-                  <CardBody>
-                  <CardTitle><h4>Comments</h4></CardTitle>
-                    {this.renderComments(dish.comments)}
-                  </CardBody>
-                </Card>
-              </div>
-            </div>
-          </div>
+          <ul className = "list-unstyled">
+            <li>{comment.comment} </li>
+            <li>-- {comment.author}, {formattedDate}</li>
+          </ul>
         );
+      });
+
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          {commentsList}
+        </div>
+      );
     }
+    else
+      return(<div></div>);
+  }
+
+  // render the DishDetail
+  render() {
+    const dish = this.props.dish; // selected dish is passed as prop
+
+    return (
+      <div className="row">
+        {this.renderDish(dish)}
+        {this.renderComments(dish.comments)}
+      </div>
+    );
+  }
 }
 
 export default DishDetail;
